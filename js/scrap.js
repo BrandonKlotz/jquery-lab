9dfba0faca964bc58b21619cae4b725d36605c1a
Brandon Klotz <brandonklotz@icloud.com>
2017-10-30 14:32:53 -0400

    Add a lot of css

---
+++ js/scrap.js
@@ -0,0 +1,48 @@
+
+  $('#table2').on('click', function() {
+    $('.form').css('display', 'flex');
+    $('#table2').addClass('.reserved');
+    $('#table-number').text('Table Number: 2');
+  });
+
+  $('#table3').on('click', function() {
+    $('.form').css('display', 'flex');
+    $('#table3').addClass('.reserved');
+    $('#table-number').text('Table Number: 3');
+  });
+
+  $('#table4').on('click', function() {
+    $('.form').css('display', 'flex');
+    $('#table4').addClass('.reserved');
+    $('#table-number').text('Table Number: 4');
+  });
+
+  $('#table5').on('click', function() {
+    $('.form').css('display', 'flex');
+    $('#table5').addClass('.reserved');
+    $('#table-number').text('Table Number: 5');
+  });
+
+  $('#table6').on('click', function() {
+    $('.form').css('display', 'flex');
+    $('#table6').addClass('.reserved');
+    $('#table-number').text('Table Number: 6');
+  });
+
+  $('#table7').on('click', function() {
+    $('.form').css('display', 'flex');
+    $('#table7').addClass('.reserved');
+    $('#table-number').text('Table Number: 7');
+  });
+
+  $('#table8').on('click', function() {
+    $('.form').css('display', 'flex');
+    $('#table8').addClass('.reserved');
+    $('#table-number').text('Table Number: 8');
+  });
+
+  $('#table9').on('click', function() {
+    $('.form').css('display', 'flex');
+    $('#table9').addClass('.reserved');
+    $('#table-number').text('Table Number: 9');
+  });
--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 $(function() {
 
 
+  $('.table').on('click', function() {
+    $('.form').css('display', 'flex');
+    $(this).addClass('.reserved');
+    $('#table-number').text('Table Number: 1');
+  });
+
+  $('#save').on('click', function() {
+    $('.form').css('display', 'none');
+    // $(this).addClass('.reserved');
+  });
+
+  $('#close').on('click', function() {
+    $('.form').css('display', 'none');
+    $(this).removeClass('.reserved');
+  });
+
+
 });
