43c7f9b574879e156e18f6d9e8250479c09434aa
Brandon Klotz <brandonklotz@icloud.com>
2017-10-31 08:52:40 -0400

    Add feature of close button not saving

--- js/script.js
+++ js/script.js
@@ -2,20 +2,23 @@ $(function() {
 
   $('.table').on('click', function reserveTable() {
 
-
+    let table = $(this);
     let tableNum = $(this).text();
     $('.form').css('display', 'flex');
     $(this).addClass('reserved');
     $('#table-number').text('Table Number: ' + tableNum);
+
+    $('#save').on('click', function() {
+      $('.form').css('display', 'none');
+    });
+
+    $('#close').on('click', function() {
+      $(table).removeClass('reserved');
+      $(table).addClass('available');
+      $('.form').css('display', 'none');
+    });
   });
 
-  $('#save').on('click', function() {
-    $('.form').css('display', 'none');
-  });
 
-  $('#close').on('click', function() {
-    $(table).removeClass('reserved');
-    $('.form').css('display', 'none');
-  });
 
 });
