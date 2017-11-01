9f8cf3cb8741bed59641264c235aee06c7769870
Brandon Klotz <brandonklotz@icloud.com>
2017-11-01 09:25:28 -0400

    Trying to revert but having a difficult time getting it back to a working state

--- js/script.js
+++ js/script.js
@@ -5,24 +5,26 @@
   let form = $('.form');
   let selectedTable = $(".selected");
 
-
   $('.available').on('click', function selectTable() { // Selects table to make reservation
+
+    if ($(this).hasClass('reserved')) {
+      return false;
+    }
 
     let tableNum = $(this).text();
 
     $(this).addClass('selected');
+    $('#table-number').text('Table Number: ' + tableNum);
 
     form.animate({width: '575px'});
     form.animate({height: '500px'});
     form.animate({top: '240px'});
-    form.css('display', 'flex');
+    form.css('display','flex');
 
-    $('#table-number').text('Table Number: ' + tableNum);
+    $('button').click(function() {
 
-    $('#save').click(function saveReservation() {
-
-      selectedTable.removeClass("selected");
       selectedTable.addClass("reserved");
+      selectedTable.removeClass("selected");
       selectedTable.removeClass("available");
 
       form.animate({height: '500px'});
@@ -30,7 +32,7 @@
 
     });
 
-    $('#close').click(function cancelReservation() {
+    $('#close').click(function() {
 
       selectedTable.removeClass('selected');
       form.animate({height: '500px'});
