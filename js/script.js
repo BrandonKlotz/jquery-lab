7106ee2817852075c5943754cf2fd97fe39c280b
Brandon Klotz <brandonklotz@icloud.com>
2017-11-01 09:35:39 -0400

    Working again.

--- js/script.js
+++ js/script.js
@@ -2,39 +2,43 @@
 
   $(".details").hide();
 
-  let form = $('.form');
-  let selectedTable = $(".selected");
+  $('.available').on('click', function selectTable() { // Selects table to make Reservation
 
-  $('.available').on('click', function selectTable() { // Selects table to make reservation
+    let form = $('.form');
 
     if ($(this).hasClass('reserved')) {
       return false;
     }
 
-    let tableNum = $(this).text();
-
     $(this).addClass('selected');
+
+    let tableNum = $(this).text();
     $('#table-number').text('Table Number: ' + tableNum);
 
+
+    // Pop Up Animation
     form.animate({width: '575px'});
     form.animate({height: '500px'});
     form.animate({top: '240px'});
     form.css('display','flex');
 
-    $('button').click(function() {
+    $('#save').click(function() { // Save Reservation
 
-      selectedTable.addClass("reserved");
-      selectedTable.removeClass("selected");
-      selectedTable.removeClass("available");
+      $(".selected").addClass('reserved');
+      $(".selected").removeClass('selected');
+      $(".selected").removeClass('available');
 
+      // Pop Up Animation
       form.animate({height: '500px'});
       form.animate({top: '-600px'}, 350, 'linear');
 
     });
 
-    $('#close').click(function() {
+    $('#close').click(function() { // Cancel Reservation
 
-      selectedTable.removeClass('selected');
+      $(".selected").removeClass('selected');
+
+      // Pop Up Animation
       form.animate({height: '500px'});
       form.animate({top: '-600px'}, 350, 'linear');
 
