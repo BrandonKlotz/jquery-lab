35a794882b033be8619c932f9ae6efb34d55e0d3
Brandon Klotz <brandonklotz@icloud.com>
2017-11-01 10:09:25 -0400

    Add more functionality

--- js/script.js
+++ js/script.js
@@ -1,50 +1,65 @@
 (function() {
 
-  $(".details").hide();
-
   $('.available').on('click', function selectTable() { // Selects table to make Reservation
-
-    let form = $('.form');
 
     if ($(this).hasClass('reserved')) {
       return false;
-    }
+    } else {
 
+    // Defines Selected Table + Table Number
     $(this).addClass('selected');
-
     let tableNum = $(this).text();
     $('#table-number').text('Table Number: ' + tableNum);
 
-
     // Pop Up Animation
-    form.animate({width: '575px'});
-    form.animate({height: '500px'});
-    form.animate({top: '240px'});
-    form.css('display','flex');
+    $('.form').animate({width: '575px'});
+    $('.form').animate({height: '500px'});
+    $('.form').animate({top: '240px'});
+    $('.form').css('display','flex');
+    }
 
-    $('#save').click(function() { // Save Reservation
+  });
 
-      $(".selected").addClass('reserved');
-      $(".selected").removeClass('selected');
-      $(".selected").removeClass('available');
+  // Save Reservation
+  $('#save').click(function() {
 
-      // Pop Up Animation
-      form.animate({height: '500px'});
-      form.animate({top: '-600px'}, 350, 'linear');
+    nameInput = $('#party-name').val();
+    phoneInput = $('#party-num').val();
+    partyInput = $('#party-size').val();
 
-    });
+    //Verify Inputs were filled...
+    if (nameInput != '' && phoneInput != '' && partyInput != '') {
 
-    $('#close').click(function() { // Cancel Reservation
+      $('#formWrap').css('display', 'none'); //Hide Form once filled out.
 
-      $(".selected").removeClass('selected');
+      //Clear form for next use.
+      $('#nameInput').val('');
+      $('#phoneInput').val('');
+      $('#partyInput').val('');
 
-      // Pop Up Animation
-      form.animate({height: '500px'});
-      form.animate({top: '-600px'}, 350, 'linear');
+      $('.selected').removeClass('selected').addClass('reserved').removeClass('available');
 
-      return false;
+      // Pop Up Animation Out
+      $('.form').animate({height: '500px'});
+      $('.form').animate({top: '-600px'}, 350, 'linear');
 
-    });
+    } else {
+      $('#alert').text('Please fill out all fields!');
+    };
+
+  });
+
+  // Cancel Reservation
+  $('#close').click(function() {
+
+    $(".selected").removeClass('selected');
+    $('#nameInput, #phoneInput, #partyInput').val('');
+
+    // Pop Up Animation Out
+    $('.form').animate({height: '500px'});
+    $('.form').animate({top: '-600px'}, 350, 'linear');
+
+    return false;
 
   });
 
